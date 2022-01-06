22a584ac86bc930801bfa19ae1d74e4726176979
Cyres Cooper <95892453+CyresCooper@users.noreply.github.com>
2022-01-06 17:50:28 -0500

    First Commit

---
+++ game.js
@@ -0,0 +1,17 @@
+var playerName = window.prompt("What is your robot's Name?");
+console.log(playerName);
+console.log ("this logs a string, goof for leaving yourslef a message");
+//this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robots's name is " + playerName);
+// this creates a function named "fight"
+
+var name = "your name";
+console.log(name);
+
+function fight() {
+window.alert("The fight has begun!")
+}
+//fight();
+
